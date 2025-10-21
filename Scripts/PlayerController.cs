049446d7370291df3b102d4aa9f934b5530d17d9
Thomas DELASSUS <delassus.thomas@orange.fr>
2025-10-21 15:57:17 +0200

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer trailWave;
+
+    private void Awake()
+    {
+        
+    }
+}
