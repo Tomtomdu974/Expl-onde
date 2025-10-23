d006e7bb97111c820c6659f6b30ab7fd2e762168
Thomas DELASSUS <delassus.thomas@orange.fr>
2025-10-23 14:33:20 +0200

    Implement color change for player on key press
    Added functionality to change player color using materials from GameManager.

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,24 +4,39 @@ public class PlayerController : MonoBehaviour
 {
     //[SerializeField] private GameObject trailWave;
     [SerializeField] private GameObject prefabToPlayer;
-    [SerializeField] private float playerSpeed = 0.5f;
     private GameManager gameManager;
-    private Rigidbody rb;
+    [SerializeField] private GameObject Wave1;
+    [SerializeField] private GameObject Wave2;
+    [SerializeField] private GameObject Wave3;
+    private int actualColorIndex = 0;
     
 
-    private void Awake()
+    private void Start()
     {
+
         //trailWave.SetActive(true);
-        rb = GetComponent<Rigidbody>();
         GameObject playerObject = Instantiate(prefabToPlayer, transform.position, Quaternion.identity);
         playerObject.transform.SetParent(transform);
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         playerObject.GetComponent<Renderer>().material = gameManager.materialsWave[0];
+        playerObject.GetComponent<Renderer>().material = gameManager.materialsWave[actualColorIndex];
+        // Wave2.GetComponent<Renderer>().material = gameManager.materialsWave[actualColorIndex + 1];
+        // Wave3.GetComponent<Renderer>().material = gameManager.materialsWave[actualColorIndex + 2];
+
     }
 
     
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            actualColorIndex += 1;
+            if (actualColorIndex > gameManager.materialsWave.Length)
+            {
+                actualColorIndex = 0;
+            }
+            Wave1.GetComponent<Renderer>().material = gameManager.materialsWave[actualColorIndex];
+            
+        }
     }
 }
