099d8d713614c83cdc8b491de0c0c7637525637e
Thomas DELASSUS <delassus.thomas@orange.fr>
2025-10-23 14:34:35 +0200

    Add files via upload

---
+++ Scripts/Collectable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onCollected;
+    [SerializeField] private string collectableTag = "Collectable";
+    private Color obstacleColor;
+
+    void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+        obstacleColor = GetComponent<Renderer>().material.color;
+    }
+     
+     private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(collectableTag) && obstacleColor == other.GetComponent<Renderer>().material.color)
+        {
+            onCollected?.Invoke();
+            Debug.Log("Collectable collected!");
+        }
+        // else
+        // {
+        //     other.gameObject.SetActive(false);
+        //     Debug.Log("Pas la bonne couleure !");
+        // }
+    }
+}
---
+++ Scripts/CollectableObstacle.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CollectableObstacle : MonoBehaviour
+{
+   [SerializeField] private IntEventChannelSO CollectedEvent;
+    [SerializeField] private int score = 100;
+
+    public void OnCollected()
+    {
+        CollectedEvent.RaiseEvent(score);
+        Destroy(gameObject);
+    }
+}
